4e870a48ba75248e6ac78908ecf863dcdcf3679a
Diego García <diego@xaethos.net>
2013-02-15 12:09:32 +0800

    Rails init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HerokuSandbox::Application.config.secret_token = 'e6b5682e97e66fc34b3bbe96870f0fc84186c9a70edc55f606fd37970f49836c5e3044465f7f3c95520b3350fcce800b2ae8582f14aa4af6e1a8e26469562f46'
